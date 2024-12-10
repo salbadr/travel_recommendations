b88aa4c507ade133faa5de969105d1a92793bf6c
Salman <mailsbadr@gmail.com>
2024-12-10 09:24:24 -0500

    finished with styling of recommendations

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 
 const submitForm = document.getElementById('submit');
 submitForm.addEventListener('submit', fetchRecommendations);
+submitForm.addEventListener('reset', clearResults);
+
+function clearResults(){
+    const searchResults = document.getElementById('search_results');
+    searchResults.innerHTML = '';
+}
 
 function fetchRecommendations(event) {
     event.preventDefault();
+    clearResults();
     const term = submitForm[0].value.toLowerCase();
 
 
@@ -14,8 +21,10 @@ function fetchRecommendations(event) {
         .then((data) => {
             for (const [key, value] of Object.entries(data)) {
                 if (key.includes(term) || term === 'country') {
-                    if (term === 'country' || term ==='countries'){
-                        handleTerms(value.cities);
+                    if (key ==='countries'){
+                        for(const country of value){
+                            handleTerms(country.cities);
+                        }
                     }
                     else{
                         handleTerms(value)
@@ -44,5 +53,7 @@ function handleTerms(list) {
 `;
         div.innerHTML = result;
         searchResults.appendChild(div);
+        searchResults.style.overflowY = 'scroll'
+
     }
 }
